4bc02de3c1a087c4b6922c3cb72d63106488f754
Vigneshwaran-PS <154402414+Vigneshwaran-PS@users.noreply.github.com>
2024-02-04 12:08:11 +0530

    If_Statement_with_boolean.java is updated

--- Control_Statement/If_Statement_with_boolean.java
+++ Control_Statement/If_Statement_with_boolean.java
@@ -5,7 +5,13 @@ public class If_Statement_with_boolean {
 
     static boolean b;//false
 
+    //main method overload
+    public static void main(){
+        System.out.println("default value of boolean b is : "+b);
+    }
+
     public static void main(String[] args) {
+        main();
         if(b){
             System.out.println("true");
         }else{
