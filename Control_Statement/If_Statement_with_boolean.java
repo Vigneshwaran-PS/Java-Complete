19c8596172ad59d4f00690335b351f0101acbad2
Vigneshwaran-PS <154402414+Vigneshwaran-PS@users.noreply.github.com>
2024-02-04 12:01:25 +0530

    If_Statement_with_boolean.java is added

---
+++ Control_Statement/If_Statement_with_boolean.java
@@ -0,0 +1,21 @@
+package Control_Statement;
+
+public class If_Statement_with_boolean {
+
+
+    static boolean b;//false
+
+    public static void main(String[] args) {
+        if(b){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+
+        if(!b){
+            System.out.println("true");
+        }else{
+            System.out.println("false");
+        }
+    }
+}
