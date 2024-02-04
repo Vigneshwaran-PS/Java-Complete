55bab86d381e1844b240d32202663891db85d1a3
Vigneshwaran-PS <154402414+Vigneshwaran-PS@users.noreply.github.com>
2024-02-04 19:04:40 +0530

    Example2.java is added

---
+++ Primitive_Datatypes_Default_Value/Example2.java
@@ -0,0 +1,33 @@
+package Primitive_Datatypes_Default_Value;
+
+public class Example2 {
+    byte a;
+    short b;
+    int c;
+    long d;
+    float e;
+    double f;
+    boolean g;
+    String h;
+    char i;
+
+    public void display(){
+        System.out.println("Default value of byte is : "+a);
+        System.out.println("Default value of short is : "+b);
+        System.out.println("Default value of int is : "+c);
+        System.out.println("Default value of long is : "+d);
+        System.out.println("Default value of float is : "+e);
+        System.out.println("Default value of double is : "+f);
+        System.out.println("Default value of boolean is : "+g);
+        System.out.println("Default value of String is : "+h);
+        System.out.println("Default value of char is : "+i);
+    }
+}
+
+class Main{
+    public static void main(String ar[]){
+        Example2 obj = new Example2();
+        obj.display();
+    }
+}
+
