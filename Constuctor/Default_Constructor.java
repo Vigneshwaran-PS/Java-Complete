81375a6e1fd0a7d0629922f599b60aacf56cef5f
Vigneshwaran-PS <154402414+Vigneshwaran-PS@users.noreply.github.com>
2024-02-05 01:04:20 +0530

    Constuctor is added

---
+++ Constuctor/Default_Constructor.java
@@ -0,0 +1,12 @@
+package Constuctor;
+
+public class Default_Constructor {
+
+    Default_Constructor(){
+        System.out.println("Default Constructor is Called");
+    }
+
+    public static void main(String[] args) {
+        Default_Constructor obj = new Default_Constructor();
+    }
+}
