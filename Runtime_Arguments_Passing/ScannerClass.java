3a219d88905ab69e53d0a4e40370f6a914baffd9
Vigneshwaran-PS <154402414+Vigneshwaran-PS@users.noreply.github.com>
2024-02-04 22:38:43 +0530

    ScannerClass.java is added

---
+++ Runtime_Arguments_Passing/ScannerClass.java
@@ -0,0 +1,21 @@
+package Runtime_Arguments_Passing;
+
+import java.util.Scanner;
+
+public class ScannerClass {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter String : ");
+        String sting = s.next();
+        System.out.println("Enter Id : ");
+        int i = s.nextInt();
+        System.out.println("Enter a Character : ");
+        char c = s.next().charAt(0);
+        System.out.println("Enter Boolean value : ");
+        Boolean b = s.nextBoolean();
+        System.out.println("Enter float number : ");
+        Float f = s.nextFloat();
+        System.out.println("Enter Double : ");
+        Double d = s.nextDouble();
+    }
+}
