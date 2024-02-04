c32ffb367e36a97977955ace90bcfd322190726e
Vigneshwaran-PS <154402414+Vigneshwaran-PS@users.noreply.github.com>
2024-02-04 22:54:43 +0530

    ScannerClass.java is updated

--- Runtime_Arguments_Passing/ScannerClass.java
+++ Runtime_Arguments_Passing/ScannerClass.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class ScannerClass {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter String : ");
-        String sting = s.next();
         System.out.println("Enter Id : ");
         int i = s.nextInt();
+        s.nextLine();
+        System.out.println("Enter String : ");
+        String sting = s.nextLine();
         System.out.println("Enter a Character : ");
         char c = s.next().charAt(0);
         System.out.println("Enter Boolean value : ");
