e7ee4dcd92ae8f504473f0a6db745552dbe92fba
Vigneshwaran-PS <154402414+Vigneshwaran-PS@users.noreply.github.com>
2024-02-04 19:29:49 +0530

    Example3.java is added

---
+++ Primitive_Datatypes_Default_Value/Example3.java
@@ -0,0 +1,26 @@
+package Primitive_Datatypes_Default_Value;
+
+public class Example3 {
+    byte a;
+    short b;
+    int c;
+    long d;
+    float e;
+    double f;
+    boolean g;
+    String h;
+    char i;
+
+    public static void main(String[] args) {
+        Example3 obj = new Example3();
+        System.out.println("Default value of byte is : "+obj.a);
+        System.out.println("Default value of short is : "+obj.b);
+        System.out.println("Default value of int is : "+obj.c);
+        System.out.println("Default value of long is : "+obj.d);
+        System.out.println("Default value of float is : "+obj.e);
+        System.out.println("Default value of double is : "+obj.f);
+        System.out.println("Default value of boolean is : "+obj.g);
+        System.out.println("Default value of String is : "+obj.h);
+        System.out.println("Default value of char is : "+obj.i);
+    }
+}
