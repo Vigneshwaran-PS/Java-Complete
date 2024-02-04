32063a85ea4ca05fe3d25258e96d2f3ad687907a
Vigneshwaran-PS <154402414+Vigneshwaran-PS@users.noreply.github.com>
2024-02-04 11:05:27 +0530

    Infinite_for_loop.java is added

---
+++ Control_Statement/Infinite_for_loop.java
@@ -0,0 +1,15 @@
+package Control_Statement;
+
+public class Infinite_for_loop {
+    public static void main(String[] args) {
+
+        for(int i=0;;){
+            System.out.println("Hello world");
+        }
+
+ //       we don't need to initialize it
+//        for(;;){
+//            System.out.println("Hello world");
+//        }
+    }
+}
