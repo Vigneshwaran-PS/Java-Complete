9d9b9f14e3da980a7b9c55cb32e59cf1b872a34c
Vigneshwaran-PS <154402414+Vigneshwaran-PS@users.noreply.github.com>
2024-02-04 09:56:11 +0530

    Example3.java is added

---
+++ Runtime_Arguments_Passing/Example3.java
@@ -0,0 +1,9 @@
+package Runtime_Arguments_Passing;
+
+public class Example3 {
+    public static void main(String args[]){
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println("a+b : "+  (a+b));
+    }
+}
