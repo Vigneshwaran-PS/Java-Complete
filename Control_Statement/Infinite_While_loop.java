334dad970a50e3734f53b35f87f73e61199d4298
Vigneshwaran-PS <154402414+Vigneshwaran-PS@users.noreply.github.com>
2024-02-04 11:02:15 +0530

    Infinite_While_loop.java is added

---
+++ Control_Statement/Infinite_While_loop.java
@@ -0,0 +1,10 @@
+package Control_Statement;
+
+public class Infinite_While_loop {
+    public static void main(String[] args) {
+
+        while(true){
+            System.out.println("Hello World");
+        }
+    }
+}
