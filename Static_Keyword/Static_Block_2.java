00de1a9a6f3ba095454edcbf7f659611fc32b5ae
Vigneshwaran-PS <154402414+Vigneshwaran-PS@users.noreply.github.com>
2024-02-05 16:07:46 +0530

    Static_Block_2.java is added

---
+++ Static_Keyword/Static_Block_2.java
@@ -0,0 +1,16 @@
+package Static_Keyword;
+
+public class Static_Block_2 {
+
+    static {
+        System.out.println("Static Block 1");
+    }
+
+    static{
+        System.out.println("Static block 2");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("From main Method");
+    }
+}
