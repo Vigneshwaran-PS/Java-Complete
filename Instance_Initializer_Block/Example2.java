a3f5a2766dd43ae1e1bccc4dd57eb200622ab1cc
Vigneshwaran-PS <154402414+Vigneshwaran-PS@users.noreply.github.com>
2024-02-05 19:10:07 +0530

    Example1.java is added

---
+++ Instance_Initializer_Block/Example2.java
@@ -0,0 +1,35 @@
+package Instance_Initializer_Block;
+
+
+class A{
+    {
+        System.out.println("Initializer Block 1");
+    }
+
+    A(){
+        System.out.println("Constructor A");
+    }
+
+    {
+        System.out.println("Initializer Block 2");
+    }
+}
+
+class B extends A{
+    {
+        System.out.println("Initializer Block 3");
+    }
+
+    B(){
+        System.out.println("Constructor B");
+    }
+
+    {
+        System.out.println("Initializer Block 3");
+    }
+}
+public class Example2 {
+    public static void main(String[] args) {
+        A obj = new B();
+    }
+}
