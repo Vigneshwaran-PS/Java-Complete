93a3948700ddfae70ab6ac83de53f4b748c5e3f9
Vigneshwaran-PS <154402414+Vigneshwaran-PS@users.noreply.github.com>
2024-02-08 20:53:10 +0530

    Ambiguity package is added

---
+++ Ambiguity/Example1.java
@@ -0,0 +1,17 @@
+package Ambiguity;
+
+public class Example1 {
+
+// this program gives ambiguity because compiler can not decide which method to invoke
+    public static void sum(int a , long b){
+        System.out.println(a+b);
+    }
+
+    public static void sum(long a , int b){
+        System.out.println(a+b);
+    }
+
+    public static void main(String[] args) {
+        sum(333,34);
+    }
+}
