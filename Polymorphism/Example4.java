54000c38f465a4edb4ae330b4a44f270263f7635
Vigneshwaran-PS <154402414+Vigneshwaran-PS@users.noreply.github.com>
2024-02-09 21:28:11 +0530

    Example4.java is addded

---
+++ Polymorphism/Example4.java
@@ -0,0 +1,31 @@
+package Polymorphism;
+
+
+class Animal{
+    void eat(){
+        System.out.println("Animal is Eating");
+    }
+}
+
+class Dog extends Animal{
+    void eat(){
+        System.out.println("Dog is Eating");
+    }
+}
+
+class BabyDog extends Animal{
+    void eat(){
+        System.out.println("BabyDog is Eating");
+    }
+}
+public class Example4 {
+    public static void main(String[] args) {
+        Animal a1, a2 ,a3;
+        a1 = new Animal();
+        a2 = new Dog();
+        a3 = new BabyDog();
+        a1.eat();;
+        a2.eat();
+        a3.eat();
+    }
+}
