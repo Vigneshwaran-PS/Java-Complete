1f6d173981623eed87f1c5fc635d1999a53e5adc
Vigneshwaran-PS <154402414+Vigneshwaran-PS@users.noreply.github.com>
2024-02-10 18:35:47 +0530

    Instanceof_operator package is added

---
+++ Instanceof_operator/Example1.java
@@ -0,0 +1,8 @@
+package Instanceof_operator;
+
+public class Example1 {
+    public static void main(String[] args) {
+        Example1 s = new Example1();
+        System.out.println(s instanceof Example1);
+    }
+}
